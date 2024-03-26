af9e8da74d1aefe40ba0f0c0cb1b1cf329a487a6
audiosupp <ivangerasimyukmail@gmail.com>
2024-03-26 22:43:18 +0300

    add: function upload to s3 bucket

--- src/components/UploadPhotoModal.vue
+++ src/components/UploadPhotoModal.vue
@@ -1,22 +1,42 @@
 <script setup>
 import { ref } from 'vue';
+import { supabase } from '@/supabase';
+
 const open = ref(false);
+const caption = ref("");
+const file = ref(null);
+
+
 const showModal = () => {
     open.value = true;
 };
-const handleOk = e => {
-    console.log(e);
-    open.value = false;
+const handleOk = async (e) => {
+    //open.value = false;
+    const fileName = Math.floor(Math.random() * 10000000000000);
+    if (file.value) {
+        const response = await supabase.storage.from("images").upload('public/' + fileName, file.value);
+        console.log({ response });
+    }
 };
+const handleUploadChange = (e) => {
+    if (e.target.files[0]) {
+        file.value = e.target.files[0];
+    }
+}
 </script>
 
 <template>
     <div>
         <a-button @click="showModal">Upload Photo</a-button>
-        <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+        <a-modal v-model:open="open" title="UploadPhoto" @ok="handleOk">
+            <input type="file" accept=".jpeg,.png" @change="handleUploadChange">
+            <a-input v-model:value="caption" placeholder="Caption..." :max-length="50"></a-input>
         </a-modal>
     </div>
 </template>
+
+<style scoped>
+input {
+    margin-top: 10px
+}
+</style>
