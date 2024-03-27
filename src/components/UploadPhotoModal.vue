3a06bc59e00032031f71f76decdd0940f2e9f162
audiosupp <ivangerasimyukmail@gmail.com>
2024-03-27 19:13:46 +0300

    edit: upload to db from profile

--- src/components/UploadPhotoModal.vue
+++ src/components/UploadPhotoModal.vue
@@ -1,7 +1,13 @@
 <script setup>
 import { ref } from 'vue';
 import { supabase } from '@/supabase';
+import { useUserStore } from '@/stores/users';
+import { storeToRefs } from 'pinia';
 
+const loading = ref(false);
+const errorMessage = ref("");
+const userStore = useUserStore();
+const { user } = storeToRefs(userStore);
 const open = ref(false);
 const caption = ref("");
 const file = ref(null);
@@ -11,12 +17,27 @@ const showModal = () => {
     open.value = true;
 };
 const handleOk = async (e) => {
-    //open.value = false;
+    loading.value = true;
     const fileName = Math.floor(Math.random() * 10000000000000);
+
     if (file.value) {
-        const response = await supabase.storage.from("images").upload('public/' + fileName, file.value);
-        console.log({ response });
+        const { data, error } = await supabase.storage.from("images").upload('public/' + fileName, file.value);
+
+        if (error) {
+            loading.value = false;
+            return errorMessage.value = "Unable to upload image";
+        }
+
+        if (data) {
+            await supabase.from("posts").insert({
+                url: data.path,
+                caption: caption.value,
+                owner_id: user.value.id
+            })
+        }
     }
+
+    loading.value = false;
 };
 const handleUploadChange = (e) => {
     if (e.target.files[0]) {
@@ -29,8 +50,14 @@ const handleUploadChange = (e) => {
     <div>
         <a-button @click="showModal">Upload Photo</a-button>
         <a-modal v-model:open="open" title="UploadPhoto" @ok="handleOk">
-            <input type="file" accept=".jpeg,.png" @change="handleUploadChange">
-            <a-input v-model:value="caption" placeholder="Caption..." :max-length="50"></a-input>
+            <div v-if="!loading">
+                <input type="file" accept=".jpeg,.png" @change="handleUploadChange">
+                <a-input v-model:value="caption" placeholder="Caption..." :max-length="50"></a-input>
+                <a-typography-text v-if="errorMessage" type="danger">{{ errorMessage }}</a-typography-text>
+            </div>
+            <div v-else class="spinner">
+                <a-spin></a-spin>
+            </div>
         </a-modal>
     </div>
 </template>
@@ -38,5 +65,11 @@ const handleUploadChange = (e) => {
 <style scoped>
 input {
     margin-top: 10px
+}
+
+.spinner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
