64ef87606604352ffe11b39b7e49f736b63f8890
audiosupp <ivangerasimyukmail@gmail.com>
2024-03-27 21:43:56 +0300

    edit: profile gallary fully working

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,25 +2,59 @@
 import Container from './Container.vue';
 import Userbar from './Userbar.vue';
 import ImageGallary from './ImageGallary.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { supabase } from '@/supabase';
+import { useRoute } from 'vue-router';
 
+const route = useRoute()
+const { username } = route.params;
+const user = ref(null);
 const posts = ref([]);
+const loading = ref(false);
+
 const addNewPost = (post) => {
     posts.value.unshift(post);
 }
+
+
+const fetchData = async () => {
+    loading.value = true;
+    const { data: userData } = await supabase.from("users").select().eq('username', username).single();
+
+    if (!userData) {
+        loading.value = false;
+        return user.value = null;
+    }
+
+    if (userData) {
+        user.value = userData;
+    }
+
+    const { data: postsData } = await supabase.from("posts").select().eq("owner_id", user.value.id);
+
+    posts.value = postsData;
+    loading.value = false;
+}
+
+onMounted(() => {
+    fetchData()
+})
 
 </script>
 
 <template>
     <Container>
-        <div class="profile-container">
-            <Userbar :key="$route.params.username" username="Ivan Koon" :userInfo="{
-                posts: 4,
-                followers: 100,
-                following: 342
+        <div class="profile-container" v-if="!loading">
+            <Userbar :key="$route.params.username" :user="user" :userInfo="{
+            posts: 4,
+            followers: 100,
+            following: 342
 
-            }" :addNewPost=addNewPost></Userbar>
+        }" :addNewPost=addNewPost></Userbar>
             <ImageGallary :posts="posts"></ImageGallary>
+        </div>
+        <div class="spinner" v-else>
+            <a-spin></a-spin>
         </div>
     </Container>
 </template>
@@ -31,5 +65,12 @@ const addNewPost = (post) => {
     flex-direction: column;
 
     padding: 20px 0;
+}
+
+.spinner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 85vh;
 }
 </style>
--- src/components/Userbar.vue
+++ src/components/Userbar.vue
@@ -9,19 +9,25 @@ const route = useRoute();
 const { username: profileUsername } = route.params;
 const userStore = useUserStore();
 const { user } = storeToRefs(userStore);
-const props = defineProps(['username', 'userInfo', "addNewPost"]);
+const props = defineProps(['user', 'username', 'userInfo', "addNewPost"]);
+
 </script>
 
 <template>
-    <div class="userbar-container">
+    <div class="userbar-container" v-if="props.user">
         <div class="top-content">
-            <a-typography-title :level="2">{{ props.username }}</a-typography-title>
+            <a-typography-title :level="2">{{ props.user.username }}</a-typography-title>
             <UploadPhotoModal v-if="user && profileUsername === user.username" :addNewPost="addNewPost" />
         </div>
         <div class="bottom-content">
             <a-typography-title :level="5">{{ props.userInfo.posts }}</a-typography-title>
             <a-typography-title :level="5">{{ props.userInfo.followers }}</a-typography-title>
             <a-typography-title :level="5">{{ props.userInfo.following }}</a-typography-title>
+        </div>
+    </div>
+    <div class="userbar-container" v-else>
+        <div class="top-content">
+            <a-typography-title :level="2">User Not Found</a-typography-title>
         </div>
     </div>
 </template>
